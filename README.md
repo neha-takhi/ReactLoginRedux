779eb5e473759e2e5f0bad5bb5c9317f22063384
neha-takhi <48908600+neha-takhi@users.noreply.github.com>
2019-07-20 17:56:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Login Sample with React Redux
+
+# Basic Features
+
+1. JSX
+2. SCSS
+3. Redux with login
+
+This sample app includes login page that includes validation and authentication of user via a local storage. After successful login ,user will redirect to home page. It also includes the basic responsive header.
+
+for login use below credentials
+
+Name: neha
+Password: 123
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = false;
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.module\.s(a|c)ss$/,
+                loader: [
+                  isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      modules: true,
+                      localIdentName: '[name]__[local]___[hash:base64:5]',
+                      camelCase: true,
+                      sourceMap: isDevelopment
+                    }
+                  },
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      sourceMap: isDevelopment
+                    }
+                  }
+                ]
+              },
+              {
+                test: /\.s(a|c)ss$/,
+                exclude: /\.module.(s(a|c)ss)$/,
+                loader: [
+                  isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      sourceMap: isDevelopment
+                    }
+                  }
+                ]
+              }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+      })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+}
